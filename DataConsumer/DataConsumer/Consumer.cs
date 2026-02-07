5ae0c6fbe903fe4de5dd27aa97e0c12381415851
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-02-07 11:47:56 +0200

    Created project - library for consuming tracker service.

---
+++ DataConsumer/DataConsumer/Consumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DataConsumer
+{
+    public class Consumer
+    {
+        // TODO: add netmq package and build class similar to app in MpTracker. It should handle connection and data acception,
+        //       allow from outside to read that data.
+        // TODO: test that class in Unity.
+    }
+}

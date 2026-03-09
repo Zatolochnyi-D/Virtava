724413238b58d62925074fbeec0f408d7cd54aee
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-03-09 13:44:38 +0200

    Temporarely added naming convention map to animatable in demo app to convert blendshape names on model to camel case that is used by default to identify names of blendshapes.

--- DemoApp/Assets/_Project/ArkitBlendshapesAnimatable.cs
+++ DemoApp/Assets/_Project/ArkitBlendshapesAnimatable.cs
@@ -1,23 +1,27 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 using Univertracker.Client;
 
 public class ArkitBlendshapesAnimatable : MonoBehaviour, IArkitBlendshapesAnimatable
 {
     [SerializeField] private SkinnedMeshRenderer _renderer;
+    [SerializeField] private TextAsset _namingConvetionsMapJson;
 
+    private Dictionary<string, string> _namingConventionsMap;
     private Dictionary<ArkitBlendshape, int> _blendshapeIndexMap;
     private IEnumerable<ArkitBlendshape> _excludeFromAnimation;
 
     void Awake()
     {
+        _namingConventionsMap = JsonConvert.DeserializeObject<Dictionary<string, string>>(_namingConvetionsMapJson.text);
         _blendshapeIndexMap = new();
 
         var mesh = _renderer.sharedMesh;
         for (int i = 0; i < mesh.blendShapeCount; i++)
         {
             var name = mesh.GetBlendShapeName(i);
-            _blendshapeIndexMap[ArkitBlendshapes.GetBlendshape(name)] = i;
+            _blendshapeIndexMap[ArkitBlendshapes.GetBlendshape(name, _namingConventionsMap)] = i;
         }
 
         // _excludeFromAnimation = ArkitBlendshapes.BlendshapesList.Where(x => !_blendshapeIndexMap.TryGetValue(x, out _));
@@ -29,10 +33,8 @@ public class ArkitBlendshapesAnimatable : MonoBehaviour, IArkitBlendshapesAnimat
 
     public void Apply(ArkitBlendshape blendshape, float value)
     {
-        // Debug.Log(blendshape);
-        // Debug.Log(_excludeFromAnimation);
         // if (_excludeFromAnimation.Contains(blendshape))
         //     return;
-        // _renderer.SetBlendShapeWeight(_blendshapeIndexMap[blendshape], value * 100f);
+        _renderer.SetBlendShapeWeight(_blendshapeIndexMap[blendshape], value * 100f);
     }
 }

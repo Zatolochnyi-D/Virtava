524f7735556bf61925f7e18f5c5a2d728904b245
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-04-06 14:37:16 +0300

    Removed all the stuff that is no longer server related (tracking results message). Tracker Server now accepts protobuf Message instead of concrete message.

--- Server/src/serverlib/tracker_server.py
+++ Server/src/serverlib/tracker_server.py
@@ -1,7 +1,7 @@
 import zmq
-from serverlib.tracking_results_pb2 import TrackingResult
+from google.protobuf.message import Message
 
-class Server:  # TODO: add logger maybe? Look up how to use one first.
+class TrackerServer:  # TODO: add logger maybe? Look up how to use one first.
     def __init__(self, port: int):
         self._context = zmq.Context()
 
@@ -9,7 +9,7 @@ class Server:  # TODO: add logger maybe? Look up how to use one first.
         self._broadcast_socket.bind(f'tcp://localhost:{port}') # TODO: handle port already in use.
         # TODO: add monitoring of connections happening - when first client connects, start tracking loop. End when last client disconnects.
 
-    def send(self, result: TrackingResult):
+    def send(self, result: Message):
         try:
             self._broadcast_socket.send(result.SerializeToString())
         except zmq.ZMQError:

524d2c61ca0ac2ada4cc4f14fdf7a19df555b36e
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-04-12 16:43:01 +0300

    Renamed fields to proper private marking.

--- VirtavaServer/src/virtava_server/connections_tracking_list.py
+++ VirtavaServer/src/virtava_server/connections_tracking_list.py
@@ -3,39 +3,39 @@ from time import time
 
 class ConnectionsTrackingList:
     def __init__(self):
-        self._connections: dict[int, int] = {}
-        self._nextPropriateId = 0
-        self._count = 0
+        self.__connections: dict[int, int] = {}
+        self.__nextPropriateId = 0
+        self.__count = 0
 
     @property
     def count(self):
-        return self._count
+        return self.__count
     
     def connection_exists(self, id: int):
-        return id in self._connections.keys()
+        return id in self.__connections.keys()
     
     def create_connection(self) -> int:
-        self._connections[self._nextPropriateId] = int(time())
-        self._nextPropriateId += 1
-        self._count += 1
-        return self._nextPropriateId - 1
+        self.__connections[self.__nextPropriateId] = int(time())
+        self.__nextPropriateId += 1
+        self.__count += 1
+        return self.__nextPropriateId - 1
 
     def remove_connection(self, id: int):
         if self.connection_exists(id):
-            del self._connections[id]
-            self._count -= 1
+            del self.__connections[id]
+            self.__count -= 1
         else:
             raise Exception("Cannot remove id not present in the connections list")
         
     def update_connection_timestamp(self, id: int):
         if self.connection_exists(id):
-            self._connections[id] = int(time())
+            self.__connections[id] = int(time())
         else:
             raise Exception("Cannot update id not present in the connections list")
         
     def remove_timed_out_connections(self, timeout: int) -> int:
         timed_out_ids = []
-        for key, value in self._connections.items():
+        for key, value in self.__connections.items():
             if int(time()) - value > timeout:
                 timed_out_ids.append(key)
         for id in timed_out_ids:
--- VirtavaServer/src/virtava_server/event.py
+++ VirtavaServer/src/virtava_server/event.py
@@ -3,13 +3,13 @@ from typing import Callable
 
 class Event:
     def __init__(self):
-        self._subscribers: list[Callable] = []
+        self.__subscribers: list[Callable] = []
 
     def subscribe(self, func: Callable):
-        self._subscribers.append(func)
+        self.__subscribers.append(func)
 
     def unsubscribe(self, func: Callable):
-        self._subscribers.remove(func)
+        self.__subscribers.remove(func)
 
     def fire(self, *args, **kwargs):
-        for func in self._subscribers: func(*args, **kwargs)
+        for func in self.__subscribers: func(*args, **kwargs)

acb63109becfc1b22791b57c8b777d82658a498b
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-05-10 13:14:44 +0300

    Added Virtava.Client package to Unity project.

--- DemoApp/Assets/_Project/ArkitBlendshapesAnimatable.cs
+++ DemoApp/Assets/_Project/ArkitBlendshapesAnimatable.cs
@@ -1,39 +1,39 @@
-using System.Collections.Generic;
-using Newtonsoft.Json;
-using UnityEngine;
-// using Univertracker.Client;
+// using System.Collections.Generic;
+// using Newtonsoft.Json;
+// using UnityEngine;
+// // using Univertracker.Client;
 
-public class ArkitBlendshapesAnimatable : MonoBehaviour//, IArkitBlendshapesAnimatable
-{
-    [SerializeField] private SkinnedMeshRenderer _renderer;
-    [SerializeField] private TextAsset _namingConvetionsMapJson;
+// public class ArkitBlendshapesAnimatable : MonoBehaviour//, IArkitBlendshapesAnimatable
+// {
+//     [SerializeField] private SkinnedMeshRenderer _renderer;
+//     [SerializeField] private TextAsset _namingConvetionsMapJson;
 
-    private Dictionary<string, string> _namingConventionsMap;
-    // private Dictionary<ArkitBlendshape, int> _blendshapeIndexMap;
+//     private Dictionary<string, string> _namingConventionsMap;
+//     // private Dictionary<ArkitBlendshape, int> _blendshapeIndexMap;
 
-    void Awake()
-    {
-        _namingConventionsMap = JsonConvert.DeserializeObject<Dictionary<string, string>>(_namingConvetionsMapJson.text);
-        // _blendshapeIndexMap = new();
+//     void Awake()
+//     {
+//         _namingConventionsMap = JsonConvert.DeserializeObject<Dictionary<string, string>>(_namingConvetionsMapJson.text);
+//         // _blendshapeIndexMap = new();
 
-        var mesh = _renderer.sharedMesh;
-        for (int i = 0; i < mesh.blendShapeCount; i++)
-        {
-            var name = mesh.GetBlendShapeName(i);
-            // _blendshapeIndexMap[ArkitBlendshapes.GetBlendshape(name, _namingConventionsMap)] = i;
-        }
-    }
+//         var mesh = _renderer.sharedMesh;
+//         for (int i = 0; i < mesh.blendShapeCount; i++)
+//         {
+//             var name = mesh.GetBlendShapeName(i);
+//             // _blendshapeIndexMap[ArkitBlendshapes.GetBlendshape(name, _namingConventionsMap)] = i;
+//         }
+//     }
 
-    // public void Apply(ArkitBlendshape blendshape, float value, bool omitIfMissing = true)
-    // {
-    //     if (omitIfMissing)
-    //     {
-    //         if (_blendshapeIndexMap.TryGetValue(blendshape, out var index))
-    //             _renderer.SetBlendShapeWeight(index, value * 100f);
-    //     }
-    //     else
-    //     {
-    //         _renderer.SetBlendShapeWeight(_blendshapeIndexMap[blendshape], value * 100f);
-    //     }
-    // }
-}
+//     // public void Apply(ArkitBlendshape blendshape, float value, bool omitIfMissing = true)
+//     // {
+//     //     if (omitIfMissing)
+//     //     {
+//     //         if (_blendshapeIndexMap.TryGetValue(blendshape, out var index))
+//     //             _renderer.SetBlendShapeWeight(index, value * 100f);
+//     //     }
+//     //     else
+//     //     {
+//     //         _renderer.SetBlendShapeWeight(_blendshapeIndexMap[blendshape], value * 100f);
+//     //     }
+//     // }
+// }

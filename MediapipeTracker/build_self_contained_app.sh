a2a8b3b98af06dc987adfe64ad283af969b207aa
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-05-26 12:41:49 +0300

    Made tracker building move it to respective Unity folder.

--- MediapipeTracker/build_self_contained_app.sh
+++ MediapipeTracker/build_self_contained_app.sh
@@ -1,3 +1,4 @@
 script_path=$(cd $(dirname $0) && pwd)
 cd $script_path
-pyinstaller src/main.py --noconfirm --onedir --name mediapipe-tracking-server --add-data "face_landmarker.task:."
+path_to_put_in="../DemoApp/Assets/StreamingAssets"
+pyinstaller src/main.py --noconfirm --onedir --name mediapipe-tracking-server --add-data "face_landmarker.task:." --distpath $path_to_put_in
